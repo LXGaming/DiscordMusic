402664d24298ef7b34e23c50f86dc11dba6af531
Alex Thomson <LXGaming@users.noreply.github.com>
2019-06-14 20:46:17 +1200

    Update dependencies

--- src/main/java/io/github/lxgaming/discordmusic/util/Reference.java
+++ src/main/java/io/github/lxgaming/discordmusic/util/Reference.java
@@ -20,7 +20,7 @@ public class Reference {
     
     public static final String APP_ID = "discordmusic";
     public static final String APP_NAME = "DiscordMusic";
-    public static final String APP_VERSION = "2.1.5";
+    public static final String APP_VERSION = "2.1.6";
     public static final String AUTHORS = "LX_Gaming";
     public static final String SOURCE = "https://github.com/LXGaming/DiscordMusic/";
     public static final String WEBSITE = "https://lxgaming.github.io/";
